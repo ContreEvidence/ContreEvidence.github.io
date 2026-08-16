359b894dd8d9fc3928c40818168401163690a0bc
ContreEvidence <contreevidence@gmail.com>
2026-08-16 12:56:37 +0200

    Restore compatibility shim for legacy orientation script path

---
+++ orientation.js
@@ -0,0 +1,7 @@
+(() => {
+  'use strict';
+  const script = document.createElement('script');
+  script.src = '/assets/orientation.js?v=20260810-13';
+  script.defer = true;
+  document.head.appendChild(script);
+})();
